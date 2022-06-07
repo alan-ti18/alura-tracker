9219fd55324c34ab2db164865b6f0f211b3c9c22
Alan Freitas <dev.alanfs@gmail.com>
2022-06-07 11:17:55 -0300

    code refactor reorganiza componentes e interface

---
+++ src/components/CFormulario.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
+            </div>
+            <div class="column">
+                <CTemporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    import CTemporizador from "./CTemporizador.vue";
+
+    export default defineComponent({
+        name: 'CFormulario',
+        components: {
+            CTemporizador
+        },
+        emits: ['aoSalvarTarefa'],
+        data () {
+            return {
+               descricao: ''
+            }
+        },
+        methods: {
+            finalizarTarefa (tempoDecorrido: number) : void {
+                this.$emit('aoSalvarTarefa', {
+                    duracaoEmSegudos: tempoDecorrido,
+                    descricao: this.descricao
+                })
+                this.descricao = '';
+            }
+        }
+    })
+</script>
+
+
+<style scoped>
+
+</style>
